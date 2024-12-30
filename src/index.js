da97f720e53700ef2bdef071bbf90c9106ec7b25
NMMalika <94176433+NMMalika@users.noreply.github.com>
2024-12-30 16:51:04 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(
+
+<React.StrictMode>
+    <h1> Hello!</h1>
+    <p>sub title</p>
+</React.StrictMode>
+);
