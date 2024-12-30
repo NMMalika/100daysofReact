c4492fb7e55344ef83892a0a70db8199f18cf78f
NMMalika <94176433+NMMalika@users.noreply.github.com>
2024-12-30 17:36:19 +0300

    progress

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDom from "react-dom/client";
+import App from "./App";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
 
 <React.StrictMode>
-    <h1> Hello!</h1>
-    <p>sub title</p>
+    <App />
 </React.StrictMode>
 );
